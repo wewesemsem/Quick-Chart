075b4e7d720e43045b8507bf1a731ea7762d93bc
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2020-05-18 16:28:57 -0400

    api

--- backend/quickChart/views.py
+++ backend/quickChart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Sending back html build soon.")
+
+def getChart(request):
+    return HttpResponse("Sending back a chart soon:)")
