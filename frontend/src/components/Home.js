4386bd8a0bd0c9f57911f86938a937d37223bc3c
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2020-05-20 16:08:59 -0400

    proxy and axios setup

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 
 class Home extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = { message: 'Oops not yet' };
+  }
+  async componentDidMount() {
+    try {
+      const message = await axios.get('/api/getChart');
+      this.setState({ message: message.data });
+    } catch (err) {
+      console.log(err);
+    }
   }
   render() {
-    return <div> HEY!</div>;
+    return <div>{this.state.message}</div>;
   }
 }
 
