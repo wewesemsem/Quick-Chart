afa0abaf6c34b900f6c6d9d51370b0044557b83f
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2020-05-21 16:01:02 -0400

    attempt 3

--- backend/settings.py
+++ backend/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, './')],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
